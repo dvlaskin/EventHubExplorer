ce4acb30eb388767eb1af9ff3d7b111ad1f82338
dvlaskin <dvlaskin.mail@gmail.com>
2026-05-17 13:16:14 +0300

    Add new message layout preset for 50/50 send/receive configuration and update related styles

--- src/Domain/Enums/MessagePageLayoutPreset.cs
+++ src/Domain/Enums/MessagePageLayoutPreset.cs
@@ -3,5 +3,6 @@ namespace Domain.Enums;
 public enum MessagePageLayoutPreset
 {
     TopSendReceiveBottomPayload,
-    LeftSendReceiveRightPayload
+    LeftSendReceiveRightPayload,
+    LeftSendRightReceiveBottomPayload
 }
